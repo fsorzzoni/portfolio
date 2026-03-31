00236c9c637830f5b972c7d59c1325900a893714
fsorzzoni <ferminsorzzoni@gmail.com>
2026-03-31 15:02:34 -0300

    Footer.jsx

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,3 +1,20 @@
+import SocialProfile from "../../ui/SocialProfile/SocialProfile.jsx";
+
 export default function Footer() {
-    return (<></>);
+    return (
+        <footer>
+            <section>
+                <h2>Contacto</h2>
+                <p>ferminsorzzoni@gmail.com</p>
+                <div>
+                    <SocialProfile type="linkedin" />
+                    <SocialProfile type="github" />
+                </div>
+            </section>
+
+            <section>
+                <p>Copyright Fermin Sorzzoni - 2026</p>
+            </section>
+        </footer>
+    );
 }
