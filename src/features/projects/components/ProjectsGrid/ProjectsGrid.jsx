3e35efd2923b1c4171fb22758dc598d02a36462a
fsorzzoni <ferminsorzzoni@gmail.com>
2026-03-31 11:28:51 -0300

    h2 Mis proyectos en ProjectsGrid

--- src/features/projects/components/ProjectsGrid/ProjectsGrid.jsx
+++ src/features/projects/components/ProjectsGrid/ProjectsGrid.jsx
@@ -3,11 +3,14 @@ import ProjectCard from "../ProjectCard/ProjectCard.jsx";
 
 export default function ProjectsGrid() {
     return (
-        <div className="grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4">
-            <ProjectCard project={PROJECTS.unitracker} />
-            <ProjectCard project={PROJECTS.portfolio} />
-            <ProjectCard project={PROJECTS.ssoo} />
-            <ProjectCard project={PROJECTS.seju} />
+        <div>
+            <h2>Mis proyectos</h2>
+            <div className="grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4">
+                <ProjectCard project={PROJECTS.unitracker} />
+                <ProjectCard project={PROJECTS.portfolio} />
+                <ProjectCard project={PROJECTS.ssoo} />
+                <ProjectCard project={PROJECTS.seju} />
+            </div>
         </div>
     );
 }
